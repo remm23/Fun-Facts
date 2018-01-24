6bd7e245ca25c0eac530bbb4a86be0dfdcbb9298
Remi Tobias <remitobias@Remis-MacBook-Pro.local>
2018-01-24 22:58:40 +0000

    App displays a fun fact when the button is tapped

---
+++ FunFacts/FactsDataModel.swift
@@ -0,0 +1,48 @@
+//
+//  FactsDataModel.swift
+//  FunFacts
+//
+//  Created by Remi Tobias on 07/01/2018.
+//  Copyright © 2018 Remi Tobias. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let funFacts: [String] = [
+    "The atoms of different elements are different from each other because they have different numbers of protons",
+    "It is a myth that it is dangerous to wake up a sleepwalker because it may cause them a heart attack, shock, brain damage, or something else.",
+    "Although some will say Please Please Me was The Beatles' first number one single, it was their next single, From Me To You, which topped the Record Retailer chart",
+    "Clouds, snow, and rain are all made of up of some form of water.",
+    "A person's ABO type is determined by the presence or absence of the A or B substance on his red cells",
+    "Covering an expanse of over 6.6 million square miles, Russia is the world's largest country by landmass, beating out runner-up Canada by around 2.8 million square miles.",
+    "In flight, the fastest bird is the spine-tailed swift of Siberia which can reach speeds in excess of 100 miles per hour",
+    "the fastest bird in the water is the Gentoo penguin, which swims at about 22 mph.",
+    "Elvis Aaron Presley was born on January 8, 1935 in East Tupelo, Mississippi",
+    "The very first restaurant in the world was opened in Paris in 1765"
+]
+
+
+let colors: [UIColor] = [
+    UIColor.init(red: 238/255, green: 152/255, blue: 174/255, alpha: 1.0),
+    UIColor.init(red: 179/255, green: 152/255, blue: 174/255, alpha: 1.0),
+    UIColor.init(red: 174/255, green: 139/255, blue: 252/255, alpha: 1.0),
+    UIColor.init(red: 213/255, green: 124/255, blue: 255/255, alpha: 1.0),
+    UIColor.init(red: 249/255, green: 150/255, blue: 255/255, alpha: 1.0),
+    UIColor.init(red: 177/255, green: 219/255, blue: 116/255, alpha: 1.0),
+    UIColor.init(red: 178/255, green: 164/255, blue: 234/255, alpha: 1.0),
+    UIColor.init(red: 234/255, green: 111/255, blue: 264/255, alpha: 1.0),
+    UIColor.init(red: 244/255, green: 95/255, blue: 95/255, alpha: 1.0),
+    UIColor.init(red: 150/255, green: 95/255, blue: 244/255, alpha: 1.0)
+]
+
+
+
+
+
+
+
+
+
+
+
--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var factButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        factButton.backgroundColor = UIColor.white
+        factButton.layer.cornerRadius = 30
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func factButtonTapped() {
+        getRandomItems()
+    }
+    
+    func getRandomItems() {
+        let randomNumber = arc4random_uniform(UInt32(funFacts.count))
+        factLabel.text = funFacts[Int(randomNumber)]
+        self.view.backgroundColor = colors[Int(randomNumber)]
+        factButton.tintColor = colors[Int(randomNumber)]
+    }
 
 }
 
